62333e6f84186c7bff4a851e18bd3546c312f85b
ossama <lazriossama18@gmail.com>
2024-04-01 12:04:20 +0000

    Add , Edit, Remove Actions solved

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,7 +1,5 @@
 <?php
 
-// src/Controller/DefaultController.php
-
 namespace App\Controller;
 use App\Entity\Task;
 use App\Form\Type\TaskType;
@@ -11,13 +9,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 
 class DefaultController extends AbstractController
 {
+
+    private $doctrine;
     private $taskRepository;
 
-    public function __construct(TaskRepository $taskRepository)
+    public function __construct(TaskRepository $taskRepository, ManagerRegistry $doctrine)
     {
+        $this->doctrine = $doctrine;
         $this->taskRepository = $taskRepository;
     }
 
@@ -58,20 +60,62 @@ class DefaultController extends AbstractController
     * @Route("/create/task", name="task_create")
     * @return Response
     */
-    public function createTask()
+    public function createTask(Request $request)
     {
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
             
         if ($form->isSubmitted() && $form->isValid()) {
             $task = $form->getData();
+            // still not create tasks in database (solved)
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
 
-            return $this->redirectToRoute('task_success');
+            return $this->redirectToRoute('index');
         }
 
         return $this->render('add.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+    * @Route("/edit/task/{id}", name="task_edit")
+    * @return Response
+    */
+    public function editTask(Task $task, Request $request)
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+            
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task = $form->getData();
+            // still not edit tasks in database (solved)
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+    * @Route("/delete/task/{id}", name="task_delete")
+    * @return Response
+    */
+    public function deleteTask(Task $task)
+    {
+        $entityManager = $this->doctrine->getManager();
+        $entityManager->remove($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('index');
     }
 
 }
